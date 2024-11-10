0d12a70bb7f2d4017ff4314fa5d57c9f95ff68af
Tareq1131 <tareqislam076@gmail.com>
2024-11-10 21:13:09 +0600

    modify package

--- app/about/page.js
+++ app/about/page.js
@@ -1,15 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
+
 import Image from "next/image";
 
 export default function About() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center py-32 bg-gray-100 dark:bg-gray-700 ">
-        <div className="w-full max-w-4xl p-8 bg-white dark:bg-gray-800 shadow-lg rounded-lg">
+        <div className="w-full max-w-4xl p-8 bg-white rounded-lg shadow-lg dark:bg-gray-800">
           <div className="flex flex-col md:flex-row">
-            <div className="w-full md:w-1/3 flex justify-center items-center mb-8 md:mb-0 ">
-              <div className="relative w-48 h-48 rounded-full overflow-hidden">
-                <Image
+            <div className="flex items-center justify-center w-full mb-8 md:w-1/3 md:mb-0 ">
+              <div className="relative w-48 h-48 overflow-hidden rounded-full">
+                <img
                   src="/logo.jpg"
                   alt="Profile"
                   layout="fill"
@@ -18,19 +19,19 @@ export default function About() {
                 />
               </div>
             </div>
-            <div className="w-full md:w-2/3 flex flex-col justify-center ">
-              <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">
+            <div className="flex flex-col justify-center w-full md:w-2/3 ">
+              <h1 className="mb-4 text-4xl font-bold text-gray-800 dark:text-white">
                 About Me
               </h1>
-              <p className="text-gray-600 dark:text-gray-50 text-lg mb-4">
+              <p className="mb-4 text-lg text-gray-600 dark:text-gray-50">
                 Hello! I am Tareq, a passionate software developer and tech
                 enthusiast. I created this blog to share my experiences, tips,
                 and tutorials on various programming languages and technologies.
-                I believe that learning should be a continuous journey, and I'm
+                I believe that learning should be a continuous journey, and I am
                 here to help others on their path to mastering the art of
                 coding.
               </p>
-              <p className="text-gray-600 dark:text-gray-50 text-lg">
+              <p className="text-lg text-gray-600 dark:text-gray-50">
                 Whether you are just starting out or looking to sharpen your
                 skills, you wll find a variety of resources and insights here.
                 Let explore the world of programming together!
@@ -41,10 +42,10 @@ export default function About() {
       </div>
 
       <section className="py-16 bg-gray-50 dark:bg-gray-800 dark:text-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-12">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="mb-12 text-center">
             <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">
-              Tareq  Journey as a Coder
+              Tareq Journey as a Coder
             </h2>
             <p className="mt-4 text-lg text-gray-600 dark:text-gray-50">
               From curious beginner to seasoned developer, here is how Tareq
@@ -52,7 +53,7 @@ export default function About() {
             </p>
           </div>
           <div className="space-y-12">
-            <div className="flex flex-col md:flex-row items-center">
+            <div className="flex flex-col items-center md:flex-row">
               <div className="md:w-1/3">
                 <img
                   src="/3.jpg"
@@ -60,8 +61,8 @@ export default function About() {
                   className="w-full rounded-lg shadow-lg"
                 />
               </div>
-              <div className="md:w-2/3 md:pl-8 mt-8 md:mt-0">
-                <h3 className="text-2xl font-bold text-gray-800 dark:text-white dark:text-white">
+              <div className="mt-8 md:w-2/3 md:pl-8 md:mt-0">
+                <h3 className="text-2xl font-bold text-gray-800 dark:text-white">
                   The Spark of Curiosity
                 </h3>
                 <p className="mt-4 text-gray-600 dark:text-gray-50">
@@ -75,7 +76,7 @@ export default function About() {
               </div>
             </div>
 
-            <div className="flex flex-col md:flex-row-reverse items-center">
+            <div className="flex flex-col items-center md:flex-row-reverse">
               <div className="md:w-1/3">
                 <img
                   src="/2.jpg"
@@ -83,13 +84,13 @@ export default function About() {
                   className="w-full rounded-lg shadow-lg"
                 />
               </div>
-              <div className="md:w-2/3 md:pr-8 mt-8 md:mt-0">
+              <div className="mt-8 md:w-2/3 md:pr-8 md:mt-0">
                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">
                   Diving Deeper
                 </h3>
                 <p className="mt-4 text-gray-600 dark:text-gray-50">
-                  After mastering the basics, Tareq s thirst for knowledge
-                  grew. He began exploring more complex topics such as data
+                  After mastering the basics, Tareq s thirst for knowledge grew.
+                  He began exploring more complex topics such as data
                   structures, algorithms, and web development. Enrolling in
                   online courses and attending coding boot-camps, Tareq quickly
                   expanded his skill set, taking on freelance projects to apply
@@ -98,7 +99,7 @@ export default function About() {
               </div>
             </div>
 
-            <div className="flex flex-col md:flex-row items-center">
+            <div className="flex flex-col items-center md:flex-row">
               <div className="md:w-1/3">
                 <img
                   src="/1.jpg"
@@ -106,7 +107,7 @@ export default function About() {
                   className="w-full rounded-lg shadow-lg"
                 />
               </div>
-              <div className="md:w-2/3 md:pl-8 mt-8 md:mt-0">
+              <div className="mt-8 md:w-2/3 md:pl-8 md:mt-0">
                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">
                   Taking on Challenges
                 </h3>
@@ -122,7 +123,7 @@ export default function About() {
               </div>
             </div>
 
-            <div className="flex flex-col md:flex-row-reverse items-center">
+            <div className="flex flex-col items-center md:flex-row-reverse">
               <div className="md:w-1/3">
                 <img
                   src="/4.jpg"
@@ -130,7 +131,7 @@ export default function About() {
                   className="w-full rounded-lg shadow-lg"
                 />
               </div>
-              <div className="md:w-2/3 md:pr-8 mt-8 md:mt-0">
+              <div className="mt-8 md:w-2/3 md:pr-8 md:mt-0">
                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">
                   Giving Back
                 </h3>
